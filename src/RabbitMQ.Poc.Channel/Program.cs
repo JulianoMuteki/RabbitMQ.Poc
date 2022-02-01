f28cee514405cf034bdfccd3176d0f550015b98a
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2022-02-01 18:32:27 -0300

    Add project files.

---
+++ src/RabbitMQ.Poc.Channel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.Poc.Channel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/RabbitMQ.Poc.Publish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.Poc.Publish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
